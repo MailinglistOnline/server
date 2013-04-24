dae3c4bc981cc726353e660e5cdea44a6469bb1d
root <root@localhost.localdomain>
2013-04-24 20:28:58 +0200

    nothing

--- export/src/main/java/EmailResource.java
+++ export/src/main/java/EmailResource.java
@@ -47,18 +47,18 @@ public class EmailResource {
         return dbClient.getAllEmails();
     }
     
-     @GET
-    @Path("/email")
+    @GET
+    @Path("/email/id")
     @Produces("application/xml")
-   // @Wrapped(element="emails")
+    @Wrapped(element="emails")
     public Email getEmailById(@QueryParam("id") String id) {
-         ArrayList<Email> list= new ArrayList<Email>();
+         //ArrayList<Email> list= new ArrayList<Email>();
          return dbClient.getEmailWithId(id);
 
     }
      
       @GET
-    @Path("/email/repliesfrom/")
+    @Path("/replies/id")
     @Produces("application/xml")
     @Wrapped(element="emails")
     public List<Email> getEmailReplies(@QueryParam("id") String id) {
@@ -68,7 +68,7 @@ public class EmailResource {
     }
     
     @GET
-    @Path("/email")
+    @Path("/from")
     @Produces("application/xml")
     @Wrapped(element="emails")
     public List<Email> getEmailByAuthor(@QueryParam("from") String author) {
